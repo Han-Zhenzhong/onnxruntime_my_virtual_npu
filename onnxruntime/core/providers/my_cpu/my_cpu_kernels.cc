aae1fcbae80c2f544b7c85c8e7b3680124170f36
Zhenzhong Han <532113006@qq.com>
2025-11-18 20:22:49 +0800

    fix use before declaration on my_cpu

--- onnxruntime/core/providers/my_cpu/my_cpu_kernels.cc
+++ onnxruntime/core/providers/my_cpu/my_cpu_kernels.cc
@@ -5,15 +5,20 @@
 #include "core/providers/my_cpu/bert/fast_gelu.h"
 #include "core/framework/op_kernel.h"
 
+// Forward declare the kernel class created by ONNX_OPERATOR_KERNEL_EX macro
+// This class is defined in fast_gelu.cc
+namespace onnxruntime {
+class kCpuExecutionProvider_FastGelu_kMSDomain_ver1;
+}
+
 namespace onnxruntime {
 namespace my_cpu {
 
 Status RegisterMyCpuKernels(KernelRegistry& kernel_registry) {
   static const BuildKernelCreateInfoFn function_table[] = {
       // FastGelu operator (using non-TYPED macro version)
-      // Class is defined in onnxruntime namespace by ONNX_OPERATOR_KERNEL_EX macro
-      ::onnxruntime::BuildKernelCreateInfo<::onnxruntime::ONNX_OPERATOR_KERNEL_CLASS_NAME(
-          kCpuExecutionProvider, kMSDomain, 1, FastGelu)>,
+      // Class is forward declared above and defined in fast_gelu.cc
+      ::onnxruntime::BuildKernelCreateInfo<::onnxruntime::kCpuExecutionProvider_FastGelu_kMSDomain_ver1>,
 
       // TODO-OPTIMIZE: [Fusion] Add fused operators for better performance
       // BuildKernelCreateInfo<ONNX_OPERATOR_KERNEL_CLASS_NAME(
